f8598ba01a1384c1ca306fa9da309b15b64d8931
Harsh Bishnoi <harshbishnoi855@gmail.com>
2025-11-05 15:48:08 +0530

    add card pin animation

--- src/app/components/CardPinAnimation.jsx
+++ src/app/components/CardPinAnimation.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 const CardPinAnimation = () => {
     return (
         <>
-            <div className="px-4">
+            <div className="px-4 min-h-screen py-5">
                 <div className="max-w-[1140px] mx-auto">
-                    <div className="">
-                        
-                    </div>
+                    <div className="bg-black h-[800px] w-full rounded-[40px] sticky top-20"></div>
+                    <div className="bg-red-300 h-[800px] w-full rounded-[40px] sticky top-30"></div>
+                    <div className="bg-green-300 h-[800px] w-full rounded-[40px] sticky top-40"></div>
+                    <div className="bg-blue-300 h-[800px] w-full rounded-[40px] sticky top-52"></div>
+                    <div className="bg-violet-300 h-[800px] w-full rounded-[40px] sticky top-52"></div>
                 </div>
             </div>
         </>
